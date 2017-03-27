6ea742c0969151b25037a9b6b72e7fb3070d0d45
parker mccurley <parkermccurlz@gmail.com>
2017-03-27 14:04:02 -0400

    Init gulpfile

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,5 +3,10 @@ import del from 'del';
 import babel from 'gulp-babel';
 
 const paths = {
-  gulpFile = './gulpfile.babel.js'
-}
+  gulpFile: './gulpfile.babel.js',
+  app: './src/js/app/**/*.js',
+  build: './js/lib/'
+};
+
+gulp.task('clean', () => del(paths.build));
+
